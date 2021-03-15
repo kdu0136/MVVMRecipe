7500393dff46b066eab2ebf292ef1baa9f7b0c6b
dongun kim <>
2021-03-15 16:51:39 +0900

    .

--- app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.mvvmrecipeapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +20,11 @@ class MainActivity : AppCompatActivity() {
                     painter = painterResource(id = R.drawable.happy_meal_small),
                     contentDescription = null,
                 )
+                Column(
+                    modifier = Modifier.padding(all = 16.dp)
+                ) {
+                    
+                }
             }
         }
     }
